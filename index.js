439c0d23d676b22d39a5ee787ed73ceb79bb055d
Pranav Kamath <pkama@udel.edu>
2023-12-15 17:22:53 -0500

    exercise 4

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
-    $('#nameform').submit(function (event) {
-        event.preventDefault();
-        var firstName = $('#fname').val();
-        var lastName = $('#lname').val();
-        $('#jumbotron').text('Name: ' + firstName + ' ' + lastName);
-    });
+  $("#nameform").submit(function (event) {
+    event.preventDefault();
+    var firstName = $("#fname").val();
+    var lastName = $("#lname").val();
+    $("#jumbotron").text("Name: " + firstName + " " + lastName);
+  });
 });
-    
